c0f7354290f278f769411686c154a82fcf158d37
Tresor-ilunga <19im065@esisalama.org>
2023-04-29 21:45:45 +0200

    fix(app) Dependencies update

--- src/Service/MetaScrapperService.php
+++ src/Service/MetaScrapperService.php
@@ -6,17 +6,19 @@ namespace App\Service;
 
 use App\ValueObject\LinkMeta;
 use Embed\Embed;
+use Throwable;
 
 final class MetaScrapperService
 {
     public function getLinkMeta(string $url): ?LinkMeta
     {
         try {
+            //$embed = new Embed();
+           // $meta = $embed->get($url);
 
             $meta = (new Embed())->get($url);
-
             return LinkMeta::fromEmbed($meta);
-        }catch (\Throwable $e) {
+        } catch (Throwable $e) {
             dd($e);
         }
     }
